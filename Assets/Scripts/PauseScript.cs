60fcb9cffc086aaf55dcf76bfb47215925aed40b
lit-haru <harutsuka0427@gmail.com>
2023-08-07 22:12:04 +0900

    DOTweenを入れた リトライボタンを押すときに効果音が鳴るのを待ってからシーン遷移するようにした

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 //ポーズ画面関連のスクリプト
 public class PauseScript : MonoBehaviour
 {
     public GameObject PauseUI;
-    
+
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
     public GameManager gameManager;
 
-    void Update()
-    {
-        
-    }
+    private bool isProcessing = false;
+
     public void PausePanel()
     {
+        audioSource.PlayOneShot(audioClip);
         PauseUI.SetActive(!PauseUI.activeSelf);
 
         if (PauseUI.activeSelf)
@@ -30,11 +33,26 @@ public class PauseScript : MonoBehaviour
 
     public void RetryButton()
     {
-        gameManager.GameStart();
+        if (!isProcessing)
+        {
+            isProcessing = true;
+            
+            SEPlay();
+            DOVirtual.DelayedCall(audioClip.length, () =>
+            {
+                gameManager.GameStart();
+                isProcessing = false;
+            });
+             
+        }
     }
     public void HomeButton()
     {
         gameManager.TitleScene();
-        
+        audioSource.PlayOneShot(audioClip);
+    }
+    public void SEPlay()
+    {
+        audioSource.PlayOneShot(audioClip);
     }
 }
