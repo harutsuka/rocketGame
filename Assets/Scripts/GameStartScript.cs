66cdbc10cd036f405c19f5ea5dd9ba782616130c
lit-haru <harutsuka0427@gmail.com>
2023-08-17 21:08:45 +0900

    設定画面を開くボタンをつくった

--- Assets/Scripts/GameStartScript.cs
+++ Assets/Scripts/GameStartScript.cs
@@ -10,21 +10,51 @@ public class GameStartScript : MonoBehaviour
     public GameManager gameManager;
     public SEPlayController SEPlayController;
 
+    public GameObject settingsUI;
+
     private bool isProcessing;
     public AudioClip ButtonAudioClip;
 
-    public void ReadyToStart()
+    public void StartButton()
     {
         if (!isProcessing)
         {
             isProcessing = true;
             SEPlayController.ButtonSE();
 
-            DOVirtual.DelayedCall(ButtonAudioClip.length - 1f, () =>
+            DOVirtual.DelayedCall(ButtonAudioClip.length - 0.8f, () =>
             {
                 gameManager.GameStart();
                 isProcessing = false;
             });
         }
-    }    
+    }
+    public void OpenSettingPanel()
+    {
+        if (!isProcessing)
+        {
+            isProcessing = true;
+            SEPlayController.ButtonSE();
+
+            DOVirtual.DelayedCall(ButtonAudioClip.length - 0.8f, () =>
+             {
+                 settingsUI.SetActive(true);
+                 isProcessing = false;
+             });
+        }
+    }
+    public void CloseSettingsPanel()
+    {
+        if (!isProcessing)
+        {
+            isProcessing = true;
+            SEPlayController.ButtonSE();
+
+            DOVirtual.DelayedCall(ButtonAudioClip.length - 0.8f, () =>
+             {
+                 settingsUI.SetActive(false);
+                 isProcessing = false;
+             });
+        }
+    }
 }
